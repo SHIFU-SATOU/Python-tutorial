fa696e8dfe4b1a4bcb2e43f4f8e8499db6e361d8
Phu Nguyen Chi <phunguyenchi2001@gmail.com>
2024-03-15 16:00:53 +0700

    Complete task 2

--- LAB01.py
+++ LAB01.py
@@ -5,3 +5,9 @@ iValue2 = int(input("b = "))
 ivalue3 = int(input("c = "))
 iValue4 = int(input("d = "))
 print(f"-Trung bình cộng: {(iValue1 + iValue2 + ivalue3 + iValue4) / 4}")
+
+#Task 2: Enter 2 integers then caculate the calculate sum, difference, product, quotient
+print("$$ Bài 2:")
+iValue1 = int(input("a = "))
+iValue2 = int(input("b = "))
+print(f"-Tổng: {iValue1 + iValue2} || Hiệu: {iValue1 - iValue2} || Tích: {iValue1 * iValue2} || Thương: {iValue1 / iValue2}")
