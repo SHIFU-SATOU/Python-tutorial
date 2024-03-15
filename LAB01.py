bcec3cf1aec4fe7e2017004e87e7e0a06ee5f8fa
Phu Nguyen Chi <phunguyenchi2001@gmail.com>
2024-03-15 15:55:49 +0700

    Complete task 1

---
+++ LAB01.py
@@ -0,0 +1,7 @@
+# Task 1: Enter 4 integers then calculate the average
+print("$$ Bài 1:")
+iValue1 = int(input("a = "))
+iValue2 = int(input("b = "))
+ivalue3 = int(input("c = "))
+iValue4 = int(input("d = "))
+print(f"-Trung bình cộng: {(iValue1 + iValue2 + ivalue3 + iValue4) / 4}")
