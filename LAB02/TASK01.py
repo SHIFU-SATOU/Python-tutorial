e6b25d51c5a74a712a0da75132b3f01ff2090901
Phu Nguyen Chi <phunguyenchi2001@gmail.com>
2024-03-27 08:26:25 +0700

    Creating redefine the output method

--- LAB02/TASK01.py
+++ LAB02/TASK01.py
@@ -3,3 +3,24 @@ class Fraction:
     def __init__(self, **kwargs):
         self.__Numerator = int(kwargs.get('Tử số'))
         self.__Denominator = int(kwargs.get('Mẫu số'))
+
+    # Processing ingredients
+    @property
+    def Numerator(self) -> int:
+        return self.__Numerator
+
+    @Numerator.setter
+    def Numerator(self, iNewValue: int) -> None:
+        self.__Numerator = iNewValue
+
+    @property
+    def Denominator(self) -> int:
+        return self.__Denominator
+
+    @Denominator.setter
+    def Denominator(self, iNewValue: int) -> None:
+        self.__Denominator = iNewValue
+
+    #Redefine the output method
+    def __str__(self):
+        return f"{self.__Numerator}/{self.__Denominator}"
