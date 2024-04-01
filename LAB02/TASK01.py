eba8922269ec261b3f0d7c53a71c1c6eb0048950
Phu Nguyen Chi <phunguyenchi2001@gmail.com>
2024-04-01 20:54:05 +0700

    Creating instance methods for fraction class in TASK01-LAB02

--- LAB02/TASK01.py
+++ LAB02/TASK01.py
@@ -1,5 +1,21 @@
 class Fraction:
     # Initialization method
     def __init__(self, numerator: int, denominator: int):
-        self.iNumerator = numerator
-        self.iDenominator = denominator
+        self.Numerator = numerator
+        self.Denominator = denominator
+
+    @property
+    def Numerator(self) -> int:
+        return self.Numerator
+
+    @Numerator.setter
+    def Numerator(self, new_value: int) -> None:
+        self.Numerator = new_value
+
+    @property
+    def Denominator(self) -> int:
+        return self.Denominator
+
+    @Denominator.setter
+    def Denominator(self, new_value: int) -> None:
+        self.Denominator = new_value
