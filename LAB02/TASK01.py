56bb495e750335018f9fa0ee73cbc3a49dc8598a
Phu Nguyen Chi <phunguyenchi2001@gmail.com>
2024-04-01 21:26:41 +0700

    Creating function to find the greatest common divisor and a method to shorten fraction in TASK01-LAB02

--- LAB02/TASK01.py
+++ LAB02/TASK01.py
@@ -27,6 +27,14 @@ class Fraction:
     def denominator(self, new_value: int) -> None:
         self.__denominator = new_value
 
+    # find greatest common divisor
+    @staticmethod
+    def __findGCD(a: int, b: int) -> int:
+        while b != 0:
+            a, b = b, a % b
+        return a
+
+
     # print fraction
     def __str__(self):
         return f"{self.__numerator}/{self.__denominator}"
@@ -39,5 +47,16 @@ class Fraction:
             new_numerator = self.__numerator + other * self.__denominator
         else:
             new_numerator = self.__numerator * other.denominator + other.numerator * self.__denominator
+            new_denominator = self.__denominator * other.denominator
+        return Fraction(new_numerator, new_denominator)
+
+    # subtract 2 fractions & subtract fraction to number
+    def __sub__(self, other) -> Fraction:
+        new_numerator = 0
+        new_denominator = 0
+        if type(int()) == type(other):
+            new_numerator = self.__numerator - other * self.__denominator
+        else:
+            new_numerator = self.__numerator * other.denominator - other.numerator * self.__denominator
             new_denominator = self.__denominator * other.denominator
         return Fraction(new_numerator, new_denominator)
