67ac6bd7f48d735e6c60d77374a845cc1215abce
Phu Nguyen Chi <phunguyenchi2001@gmail.com>
2024-04-02 23:28:31 +0700

    Creating method to find type of triangle in TASK03-LAB02

--- LAB02/TASK03.py
+++ LAB02/TASK03.py
@@ -5,11 +5,29 @@ class Triangle:
 class Triangle:
 
     # Constructor
-    def __init__(self, a, b, c):
+    def __init__(self, a, b, c) -> None:
         self.__a = a
         self.__b = b
         self.__c = c
 
     # print triangle
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.__a}, {self.__b}, {self.__c}"
+
+    # find type of triangle
+    def findTypeOfTriangle(self) -> str:
+        isRight = (self.__a ** 2 + self.__b ** 2 == self.__c ** 2 or self.__b ** 2 + self.__c ** 2 == self.__a ** 2 or
+                   self.__c ** 2 + self.__a ** 2 == self.__b ** 2)
+        isIsosceles = self.__a == self.__b or self.__b == self.__c or self.__c == self.__a
+        isEquilateral = self.__a == self.__b and self.__b == self.__c
+        if isRight:
+            if isIsosceles:
+                return "Tam giác vuông cân"
+            else:
+                return "Tam giác vuông"
+        elif isIsosceles:
+            return "Tam giác cân"
+        elif isEquilateral:
+            return "Tam giác đều"
+        else:
+            return "Tam giác bình thường"
