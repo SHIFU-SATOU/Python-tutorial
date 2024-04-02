20294582468b5ae09f576a547809bf8d09162d84
Phu Nguyen Chi <phunguyenchi2001@gmail.com>
2024-04-02 13:50:57 +0700

    Creating instance method for coordinates class in TASK02-LAB02

--- LAB02/TASK02.py
+++ LAB02/TASK02.py
@@ -1,5 +1,22 @@
 class Coordinates:
     # Initialization method
-    def __init__(self, latitude, longitude):
+    def __init__(self, latitude: int, longitude: int) -> None:
         self.__latitude = latitude
         self.__longitude = longitude
+
+    # Instance method
+    @property
+    def latitude(self) -> int:
+        return self.__latitude
+
+    @latitude.setter
+    def latitude(self, new_value: int) -> None:
+        self.__latitude = new_value
+
+    @property
+    def longitude(self) -> int:
+        return self.__longitude
+
+    @longitude.setter
+    def longitude(self, new_value: int) -> None:
+        self.__latitude = new_value
