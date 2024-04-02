5a28b9b393fda100c48e7cce2457c5c906b02054
Phu Nguyen Chi <phunguyenchi2001@gmail.com>
2024-04-02 23:04:47 +0700

    Creating method to print info of triangle in TASK03-LAB02

--- LAB02/TASK03.py
+++ LAB02/TASK03.py
@@ -1,5 +1,7 @@
 class Triangle:
     pass
+
+
 class Triangle:
 
     # Constructor
@@ -7,3 +9,7 @@ class Triangle:
         self.__a = a
         self.__b = b
         self.__c = c
+
+    # print triangle
+    def __str__(self):
+        return f"{self.__a}, {self.__b}, {self.__c}"
