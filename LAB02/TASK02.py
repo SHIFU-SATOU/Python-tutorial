34710771fd55dced86ae3d92c64804b05445b414
Phu Nguyen Chi <phunguyenchi2001@gmail.com>
2024-04-02 14:15:43 +0700

    Creating find distance method of 2 coordinates in TASK02-LAB02

--- LAB02/TASK02.py
+++ LAB02/TASK02.py
@@ -1,3 +1,10 @@
+import math
+
+
+class Coordinates:
+    pass
+
+
 class Coordinates:
     # Initialization method
     def __init__(self, latitude: int, longitude: int) -> None:
@@ -25,3 +32,15 @@ class Coordinates:
     def __str__(self) -> str:
         return f"x: {self.__latitude}, y: {self.__longitude}"
 
+    # Move to coordinates
+    def moveToCoordinates(self, distance: int) -> Coordinates:
+        return Coordinates(self.__latitude + distance, self.__longitude + distance)
+
+    # Caculate distance
+    @classmethod
+    def findDistance(cls, A: Coordinates, B: Coordinates) -> float:
+        math.sqrt((A.latitude - B.latitude) ** 2 + (A.longitude - B.longitude) ** 2)
+
+
+if __name__ == '__main__':
+    print("Hello task 2")
