30b3b93a3f19204696d70f5dbf24b20fc17ac063
Phu Nguyen Chi <phunguyenchi2001@gmail.com>
2024-04-03 08:46:35 +0700

    Creating method to check intersection of 2 circle in TASK04-LAB02

--- LAB02/TASK04.py
+++ LAB02/TASK04.py
@@ -4,10 +4,23 @@ from TASK02 import Coordinates
 
 
 class Circle:
+    pass
+
+
+class Circle:
     # constructor
     def __init__(self, r: int, x: int, y: int) -> None:
         self.__O = Coordinates(x, y)
         self.__r = r
+
+    # instance method
+    @property
+    def O(self) -> Coordinates:
+        return self.__O
+
+    @property
+    def r(self) -> int:
+        return self.__r
 
     # print info of circle
     def __str__(self) -> str:
@@ -30,3 +43,17 @@ class Circle:
     def zoomOutCircleByPercent(self, percent: int) -> float:
         p = self.caculatePerimeter()
         return p - p * (percent / 100)
+
+    # Check if this circle intersects with another circle?
+    def checkIntersects(self, other: Circle) -> bool:
+        isTwoCirclesIntersectsion = False
+        isTwoCirclesTouchInside = False
+        Distance = Coordinates(self.__O, other.O)
+        if self.__r > other.r:
+            isTwoCirclesIntersectsion = Distance > self.__r - other.r and Distance < self.__r + other.r
+            isTwoCirclesTouchInside = Distance == self.__r - other.r
+        elif self.__r < other.r:
+            isTwoCirclesIntersectsion = Distance > other.r - self.__r and Distance < other.r - self.__r
+            isTwoCirclesTouchInside = Distance == other.r - self.__r
+        isTwoCirclesTouchOutside = Distance == self.__r + other.r
+        return isTwoCirclesIntersectsion or isTwoCirclesTouchInside or isTwoCirclesTouchOutside
