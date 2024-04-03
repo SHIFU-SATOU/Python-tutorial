6dd7bfbfb5990708ead8899ad0d3eab143f03d0b
Phu Nguyen Chi <phunguyenchi2001@gmail.com>
2024-04-03 15:27:20 +0700

    Creating random methods in TASK05-LAB02

--- LAB02/TASK05.py
+++ LAB02/TASK05.py
@@ -1,5 +1,6 @@
 import math
 import re
+import random
 
 
 class Staff:
@@ -8,11 +9,12 @@ class Staff:
     # Construtor
     def __init__(self, **kwargs):
         self.__ID = kwargs.get('id')
-        self.__Name = kwargs.get('name')
-        self.__BaseSalary = kwargs.get('base salary')
-        self.__Products = kwargs.get('products')
-        self.__MonthlySalary = kwargs.get('monthly salary')
+        self.__Name = kwargs.get('name', "Chưa bổ sung")
+        self.__BaseSalary = kwargs.get('base salary', 0.0)
+        self.__Products = kwargs.get('products', 0)
+        self.__MonthlySalary = kwargs.get('monthly salary', 0.0)
 
+    # auto create id
     @classmethod
     def createID(cls):
         iNth = re.findall(r"[0-9]+", cls.__Flag)
@@ -21,4 +23,28 @@ class Staff:
             cls.__Flag = 'NV0' + str(iNextNth)
         elif (iNextNth > 9):
             cls.__Flag = 'NV' + str(iNextNth)
+
+    # auto create name
+    @staticmethod
+    def createRandomName(cls) -> str:
+        lLastNames = ['Nguyễn', 'Trần', 'Phạm', 'Hoàng', 'Bùi', 'Trịnh', 'Đặng', 'Vũ', 'Đồng']
+        lFirstNames = ['Phú', 'Tân', 'Quân', 'Hậu', 'Lộc', 'Sơn', 'Khang', 'Quyên', 'Uyên', 'Tú', 'An', 'Bích', 'Duyên']
+        lMiddleNames = ['', 'Thị', 'Chí', 'Minh', 'Đăng', 'Kim', 'Đức']
+        return random.choice(lFirstNames) + ' ' + random.choice(lLastNames) + ' ' + random.choice(lMiddleNames)
+
+    @staticmethod
+    def createRandomBaseSalary(cls) -> float:
+        return round(random.uniform(3000000, 5000000), 3)
+
+    @staticmethod
+    def createRandomTypeStaff(cls) -> str:
+        return random.choice(["Văn phòng", "Bán hàng"])
+
+    @staticmethod
+    def createRandomNumberDays() -> int:
+        return random.randint(24, 26)
+
+    @staticmethod
+    def createRandomNumberProducts() -> int:
+        return random.randint(100, 200)
 
