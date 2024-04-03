a874af0256d000bbe96a549239ad60105050d763
Phu Nguyen Chi <phunguyenchi2001@gmail.com>
2024-04-03 15:11:42 +0700

    Creating class method to create staff id in TASK05-LAB02

--- LAB02/TASK05.py
+++ LAB02/TASK05.py
@@ -1,8 +1,24 @@
+import math
+import re
+
+
 class Staff:
-    #Construtor
+    __Flag = 'NV00'
+
+    # Construtor
     def __init__(self, **kwargs):
         self.__ID = kwargs.get('id')
         self.__Name = kwargs.get('name')
         self.__BaseSalary = kwargs.get('base salary')
         self.__Products = kwargs.get('products')
         self.__MonthlySalary = kwargs.get('monthly salary')
+
+    @classmethod
+    def createID(cls):
+        iNth = re.findall(r"[0-9]+", cls.__Flag)
+        iNextNth = int(iNth[0]) + 1
+        if iNextNth in range(10):
+            cls.__Flag = 'NV0' + str(iNextNth)
+        elif (iNextNth > 9):
+            cls.__Flag = 'NV' + str(iNextNth)
+
