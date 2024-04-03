8eb25f6f7f25698f6c46ba015388b61a61ba5eea
Phu Nguyen Chi <phunguyenchi2001@gmail.com>
2024-04-03 07:45:56 +0700

    Creating method to caculate perimeter of circle in TASK04-LAB02

--- LAB02/TASK04.py
+++ LAB02/TASK04.py
@@ -1,9 +1,15 @@
+import math
+
+
 class Circle:
     # constructor
     def __init__(self, r: int) -> None:
         self.__r = r
 
     # print info of circle
-    def __str__(self)->str:
+    def __str__(self) -> str:
         return f"r = {self.__r}"
 
+    # caculate perimeter
+    def __caculatePerimeter(self) -> float:
+        return round(2 * math.pi * self.__r, 3)
