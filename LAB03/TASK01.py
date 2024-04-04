5e61385e7db9dba6561f10aa3118d11f6ad29b78
Phu Nguyen Chi <phunguyenchi2001@gmail.com>
2024-04-04 17:21:19 +0700

    Creating staff class with constructor in TASK01-LAB03

---
+++ LAB03/TASK01.py
@@ -0,0 +1,12 @@
+import math
+import random
+import re
+class Staff:
+    pass
+
+class Staff:
+    def __init__(self, **kwargs):
+        self.__ID = kwargs.get('id')
+        self.__Name = kwargs.get('name', 'Trống')
+        self.__Salary = kwargs.get('salary', 0.0)
+        self.__MonthlySalary = kwargs.get('monthly_salary', 0.0)
