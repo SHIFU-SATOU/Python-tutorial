b0cc7a05d9c9013bd7aae3d7d9b9cacc9dde9f55
Phu Nguyen Chi <phunguyenchi2001@gmail.com>
2024-04-04 17:24:44 +0700

    Creating instance methods for staff class in TASK01-LAB03

--- LAB03/TASK01.py
+++ LAB03/TASK01.py
@@ -1,12 +1,37 @@
 import math
 import random
 import re
+
+
 class Staff:
     pass
 
+
 class Staff:
+    # Constructor
     def __init__(self, **kwargs):
         self.__ID = kwargs.get('id')
         self.__Name = kwargs.get('name', 'Trống')
         self.__Salary = kwargs.get('salary', 0.0)
         self.__MonthlySalary = kwargs.get('monthly_salary', 0.0)
+
+    # Instance methods
+    # Get ID of staff
+    @property
+    def ID(self) -> str:
+        return self.__ID
+
+    # Get Name of staff
+    @property
+    def Name(self) -> str:
+        return self.__Name
+
+    # Get salary of staff
+    @property
+    def Salary(self) -> float:
+        return self.__Salary
+
+    # Get monthly salary of staff
+    @property
+    def MonthlySalary(self) -> float:
+        return self.__MonthlySalary
