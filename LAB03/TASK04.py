2c765bfc92e6ac8298bfe35472a1aea0078ab31a
Phu Nguyen Chi <phunguyenchi2001@gmail.com>
2024-04-08 15:32:39 +0700

    Create methods to print info and caculate perimeter of circle in TASK04/LAB03

--- LAB03/TASK04.py
+++ LAB03/TASK04.py
@@ -1,12 +1,24 @@
 from abc import ABC, abstractmethod
+import math
+
+
 class Point(ABC):
     # Constructor
     def __init__(self, **kwargs):
         self._X = kwargs.get('X', 0)
         self._Y = kwargs.get('Y', 0)
 
+
 class Circle(Point):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self._Radius = kwargs.get('Radius', 0)
+
+    # Print info of circle
+    def __str__(self) -> str:
+        return f"Tâm O: ({self._X}, {self._Y})| Bán kính: {self._Radius}"
+
+    # Caculate perimeter
+    def caculatePerimeter(self) -> float:
+        return round(2 * math.pi * self._Radius, 2)
 
