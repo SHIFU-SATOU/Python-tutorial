1e8ae14b821df5c7ccea278580fe93b3f1f129e7
Phu Nguyen Chi <phunguyenchi2001@gmail.com>
2024-04-08 08:11:41 +0700

    Creating class Expert with constructor in TASK02/LAB03

--- LAB03/TASK02.py
+++ LAB03/TASK02.py
@@ -8,9 +8,14 @@ class Staff(ABC):
     __Staffs = []
     # Constructor
     def __init__(self, **kwargs):
-        self.__ID = kwargs.get('id')
-        self.__Name = kwargs.get('name', "Trống")
-        self.__Salary = kwargs.get('salary', 0.0)
-        self.__ResponsibilityIndex = kwargs.get('responsibility_index', 0.0)
-        self.__MonthlySalary = kwargs.get('monthly_salary', 0.0)
+        self._ID = kwargs.get('id')
+        self._Name = kwargs.get('name', "Trống")
+        self._Salary = kwargs.get('salary', 0.0)
+        self._ResponsibilityIndex = kwargs.get('responsibility_index', 0.0)
+        self._MonthlySalary = kwargs.get('monthly_salary', 0.0)
 
+class Expert(Staff):
+    # Constructor
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.__OvertimeHours = kwargs.get('overtime_hours', 0)
