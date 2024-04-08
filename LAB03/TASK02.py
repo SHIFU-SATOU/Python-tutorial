7ea39931dfe25fa0a5686498ef8684dc80676eaf
Phu Nguyen Chi <phunguyenchi2001@gmail.com>
2024-04-08 08:50:42 +0700

    Creating methods to get ID, get monthly salary and caculate monthly salary of staff TASK02/LAB03

--- LAB03/TASK02.py
+++ LAB03/TASK02.py
@@ -16,9 +16,24 @@ class Staff(ABC):
         self._ResponsibilityIndex = kwargs.get('responsibility_index', 0.0)
         self._MonthlySalary = kwargs.get('monthly_salary', 0.0)
 
+    # Get ID of staff
+    @property
+    def ID(self) -> str:
+        return self._ID
+
+    # Get monthly salary of staff
+    @property
+    def salary(self) -> float:
+        return self._Salary
+
     # Print info of staff
     @abstractmethod
     def __str__(self) -> str:
+        pass
+
+    # Caculate monthly salary
+    @abstractmethod
+    def caculateMonthlySalary(self) -> None:
         pass
 
 
@@ -31,6 +46,9 @@ class Expert(Staff):
     def __str__(self) -> str:
         return f"ID: {self._ID}, Tên: {self._Name}, Lương: {self._Salary}, Trách nhiệm: {self._ResponsibilityIndex}, Tăng ca: {self.__OvertimeHours}, Lương tháng: {self._MonthlySalary}"
 
+    def caculateMonthlySalary(self) -> None:
+        self._MonthlySalary = self._Salary + self._Salary * self._ResponsibilityIndex + self.__OvertimeHours * 180000
+
 
 class Researcher(Staff):
     # Constructor
@@ -42,6 +60,10 @@ class Researcher(Staff):
     def __str__(self) -> str:
         return f"ID: {self._ID}, Tên: {self._Name}, Lương: {self._Salary}, Trách nhiệm: {self._ResponsibilityIndex}, Phát minh: {self.__NumberInvention}, Lương tháng: {self._MonthlySalary}"
 
+    def caculateMonthlySalary(self) -> None:
+        self._MonthlySalary = self._Salary + self._Salary * (
+                    self._ResponsibilityIndex - 0.2) + self.__NumberInvention * 5500000
+
 
 class Manager(Staff):
     # Constructor
@@ -52,10 +74,16 @@ class Manager(Staff):
     def __str__(self) -> str:
         return f"ID: {self._ID}, Tên: {self._Name}, Lương: {self._Salary}, Trách nhiệm: {self._ResponsibilityIndex}, Kiêm nhiệm: {self.__ConcurrentCoefficient}, Lương tháng: {self._MonthlySalary}"
 
+    def caculateMonthlySalary(self) -> None:
+        self._MonthlySalary = self._Salary * 0.7 + self._Salary * self._ResponsibilityIndex + self._Salary * self.__ConcurrentCoefficient
+
 if __name__ == '__main__':
     StaffA = Expert(id=123, name="Nguyen A", salary='4_500_000', responsibility_index=0.5, overtime_hours=50)
+    StaffA.caculateMonthlySalary()
     print(StaffA)
     StaffB = Researcher(id=124, name="Nguyen B", salary='5_600_000', responsibility_index=1.2, number_invention=10)
+    StaffB.caculateMonthlySalary()
     print(StaffB)
     StaffC = Manager(id=125, name="Nguyen C", salary='7_800_800', responsibility_index=1.5, concurrent_coefficient=1.3)
+    StaffC.caculateMonthlySalary()
     print(StaffC)
