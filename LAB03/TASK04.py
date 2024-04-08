5dd5a3297e32c885924c448a3dddddd809527a84
Phu Nguyen Chi <phunguyenchi2001@gmail.com>
2024-04-08 15:29:10 +0700

    Creating circle class with constructor in TASK04/LAB03

--- LAB03/TASK04.py
+++ LAB03/TASK04.py
@@ -5,3 +5,8 @@ class Point(ABC):
         self._X = kwargs.get('X', 0)
         self._Y = kwargs.get('Y', 0)
 
+class Circle(Point):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._Radius = kwargs.get('Radius', 0)
+
