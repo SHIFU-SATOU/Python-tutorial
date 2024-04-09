a004a04f873f54db2aae259b5f63e56032163b52
Phu Nguyen Chi <phunguyenchi2001@gmail.com>
2024-04-09 13:34:31 +0700

    Create clinic class, doctor class, patient class, medicine class with their constructors in TASK07/LAB03

---
+++ LAB03/TASK07.py
@@ -0,0 +1,33 @@
+class Clinic:
+    # Constructor
+    def __init__(self, **kwargs):
+        self.__ID = str(kwargs.get('ID'))
+        self.__Name = kwargs.get('Name', 'Trống')
+        self.__Doctors = []
+        self.__Patients = []
+        self.__Medicines = []
+
+
+class Doctor:
+    # Constructor
+    def __init__(self, **kwargs):
+        self.__ID = str(kwargs.get('ID'))
+        self.__Name = kwargs.get('Name', 'Trống')
+        self.__Patients = []
+
+
+class Patient:
+    # Constructor
+    def __init__(self, **kwargs):
+        self.__ID = str(kwargs.get('ID'))
+        self.__Name = kwargs.get('Name', 'Trống')
+        self.__Doctors = []
+        self.__Medicines = []
+
+
+class Medicine:
+    # Constructor
+    def __init__(self, **kwargs):
+        self.__ID = str(kwargs.get('ID'))
+        self.__Name = kwargs.get('Name', 'Trống')
+        self.__Price = kwargs.get('Price', 0.0)
